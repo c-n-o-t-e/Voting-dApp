28b5adf0ea363a811649ceb5774e883f50038454
C-note <dongustavo20@gmail.com>
2020-06-18 12:00:58 +0100

    Updates

---
+++ precache-manifest.1de661f9d98ba76c9badf7c9d9a964cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8c4cbe9d4a8039816cc4ab6b68489d",
+    "url": "/voting-dapp/index.html"
+  },
+  {
+    "revision": "eadfab03da9c6c222c64",
+    "url": "/voting-dapp/static/css/main.2dc9eb55.chunk.css"
+  },
+  {
+    "revision": "9a586c9da98b90dd0b5c",
+    "url": "/voting-dapp/static/js/2.7106e48d.chunk.js"
+  },
+  {
+    "revision": "080ae872e1244935c80a18351e363057",
+    "url": "/voting-dapp/static/js/2.7106e48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadfab03da9c6c222c64",
+    "url": "/voting-dapp/static/js/main.c7ba96e5.chunk.js"
+  },
+  {
+    "revision": "eddd366beb27d093f504",
+    "url": "/voting-dapp/static/js/runtime-main.f9f9b8c2.js"
+  }
+]);
